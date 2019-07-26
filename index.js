388731a9be02c35b0b5fc01e9079d090931890d5
Aditi Ohri <didiohri@gmail.com>
2019-07-26 01:15:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (!line.length) {
 
 const linelist = [];
 for (let i=0, l=line.length; i<l; i++) {
-  linelist.push(`${i+1}. ${line[i]}, `)
+  linelist.push(`${i+1}. ${line[i]}`)
 }
 return `The line is currently ${[linelist]}`
 }
