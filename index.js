89fbbee608a663363bdfaba4ffa33f169c1d8302
Aditi Ohri <didiohri@gmail.com>
2019-07-26 01:14:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,10 @@ function currentLine(line) {
 if (!line.length) {
   return `The line is currently empty.`
 }
-return `The line is currently ${[line]}`
+
+const linelist = [];
+for (let i=0, l=line.length; i<l; i++) {
+  linelist.push(`${i+1}. ${line[i]}, `)
+}
+return `The line is currently ${[linelist]}`
 }
