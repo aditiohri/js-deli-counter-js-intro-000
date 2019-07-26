9290932f1f393bd0f028743a2a6e2189dd1d68c2
Aditi Ohri <didiohri@gmail.com>
2019-07-26 00:56:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ deliLine.push(name);
 return `Welcome, ${name}. You are number ${deliLine.length} in line.`
 }
 
-function nowServing() {
-
+function nowServing(katzDeliLine) {
+console.log(`katzDeliLine[0]`);
+katzDeliLine.shift;
+if (katzDeliLine.length = 0) {
+  return `There is nobody waiting to be served!`
+}
 }
 
 function currentLine() {
