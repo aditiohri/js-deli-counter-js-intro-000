8fabc3064e83ed95f71ef4f9f1c0e45a53d61ad8
Aditi Ohri <didiohri@gmail.com>
2019-07-26 01:02:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ return `Welcome, ${name}. You are number ${deliLine.length} in line.`
 function nowServing(deliLine) {
 console.log(`Currently serving ${deliLine[0]}.`);
 deliLine.shift;
-if (deliLine.length = 0) {
+if (!deliLine.length) {
   console.log(`There is nobody waiting to be served!`)
 }
 }
