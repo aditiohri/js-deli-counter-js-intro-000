cd820f43cced959bb5e6c5af888debaf0099d8b1
Aditi Ohri <didiohri@gmail.com>
2019-07-26 01:18:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ const linelist = [];
 for (let i=0, l=line.length; i<l; i++) {
   linelist.push(`${i+1}. ${line[i]}`)
 }
-return `The line is currently ${[linelist.join(', ')]}`
+return `The line is currently: ${[linelist.join(', ')]}`
 }
